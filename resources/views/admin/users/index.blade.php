f39feaa970e90d2948236a7551c0c909e4d72d5a
Dark <Infra@BBDNOTFS013.linx-inves.com.br>
2017-08-22 18:50:50 -0300

    Alterando o estilo da tabela

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,7 +7,18 @@
             {!! Button::primary('Novo usuário')->asLinkTo(route('admin.users.create')) !!}
         </div>
         <div class="row">
-            {!! Table::withContents($users->items()) !!}
+            {!! 
+            Table::withContents($users->items())
+                ->hover()
+                ->condensed()
+                ->bordered()
+                ->callback('Ações', function($field, $model){
+                    $linkEdit = route('admin.users.edit', ['user' => $model->id]);
+                    $linkShow = route('admin.users.show', ['user' => $model->id]);
+                    return Button::link('Editar')->asLinkTo($linkEdit). ' | '
+                          .Button::link('Ver')->asLinkTo($linkShow);
+                })
+            !!}
         </div>
         {!! $users->links() !!}
     </div>
